8809f3ea72bd450f3c74e1477fcc78e70681427a
KrzysiekKuczma <kuczma.krzysiek@gmail.com>
2022-11-23 17:20:49 +0000

    Fixed token contract

--- contracts/zk_governance_token/lib.rs
+++ contracts/zk_governance_token/lib.rs
@@ -1,11 +1,11 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(min_specialization)]
 
-use openbrush::contracts::psp22::PSP22;
 #[openbrush::contract]
 pub mod my_psp22 {
     // imports from ink!
 	use ink_storage::traits::SpreadAllocate;
+	use ink_prelude::string::String;
 	use openbrush::contracts::ownable::*;
     
     // imports from openbrush
@@ -43,13 +43,13 @@ pub mod my_psp22 {
     
     impl AlephVote {
         #[ink(constructor)]
-        pub fn new(initial_supply: Balance) -> Self {
+        pub fn new(initial_supply: Balance, name: Option<String>, symbol: Option<String>, decimals: u8) -> Self {
             ink_lang::codegen::initialize_contract(|_instance: &mut AlephVote|{
 				_instance._mint(_instance.env().caller(), initial_supply).expect("Should mint"); 
 				_instance._init_with_owner(_instance.env().caller());
-				_instance.metadata.name = Some("alephVoteVer1".to_string());
-				_instance.metadata.symbol = Some("AV1".to_string());
-				_instance.metadata.decimals = 18;
+				_instance.metadata.name = name;
+				_instance.metadata.symbol = symbol;
+				_instance.metadata.decimals = decimals;
 			})
         }
     }
@@ -61,7 +61,7 @@ mod tests {
 
     #[ink::test]
     fn total_supply_works() {
-        let test_token = my_psp22::AlephVote::new(1000);
+        let test_token = my_psp22::AlephVote::new(1000, Some("Test Vote Token".to_string()), Some("TVT".to_string()), 18);
         assert_eq!(test_token.total_supply(), 1000);
     }
 }
