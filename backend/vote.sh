925dc2532bddc86a114871a40f73c6b69a0a0dcc
jsk28 <jsk28@sussex.ac.uk>
2022-11-24 09:45:23 +0000

    fixes for Front-end

--- backend/vote.sh
+++ backend/vote.sh
@@ -3,8 +3,14 @@
 #seed = $2;
 #option = $3; (for/against address)
 
-DEPOSIT_ID = cargo run --release -- --seed $1 show-assets 50 | tr '\n' ' ' | sed -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g' | tr -s ' ' | cut -d " " -f 3
+cd /home/jan/tmp/blend2/blender-cli/
 
-cargo run --release -- --seed $1 deposit 50 $2
+pwd
 
-cargo run --release -- --seed $1 withdraw --recipient $3 --deposit-id DEPOSIT_ID --amount $2
+sh ./vote.sh $1 $2 $3
+
+# DEPOSIT_ID = cargo run --release -- --seed $1 show-assets 50 | tr '\n' ' ' | sed -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g' | tr -s ' ' | cut -d " " -f 3
+# 
+# cargo run --release -- --seed $1 deposit 50 $2
+# 
+# cargo run --release -- --seed $1 withdraw --recipient $3 --deposit-id DEPOSIT_ID --amount $2
